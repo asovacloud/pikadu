9a0d95427de5d3e888e51f7f58c60fca50d25efc
Andrew Sova <asovacloud@gmail.com>
2020-11-18 09:38:52 +0200

    Work with JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from  'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Yo, buddy.</h1>
+      <h1>Yo, buddy...</h1>
     </div>
   );
+
+  // return React.createElement(
+  //   'div',
+  //   {
+  //     className: 'App'
+  //   },
+  //   React.createElement(
+  //     'h1',
+  //     null,
+  //     'Yo, buddy!!!!'
+  //   )
+  // );
 }
 
 export default App;
