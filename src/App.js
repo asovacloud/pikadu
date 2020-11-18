b02ebae98d4e642d15a11675cee0c5b1fab1f8ee
Andrew Sova <asovacloud@gmail.com>
2020-11-18 21:51:47 +0200

    Send children

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
     <div style={divStyle}>
       <h1>Yo, buddy...</h1>
 
-      <Car name={'BMW'} year={2021} />
-      <Car name={'Audi'} year={2019} />
+      <Car name={'BMW'} year={2021}>
+        <p style={{color: 'blue'}}>COLOR</p>
+      </Car>
+      <Car name={'Audi'} year={2019}>
+        <p style={{color: 'tomato'}}>COLOR</p>
+      </Car>
       <Car name={'Mazda'} year={2010} />
     </div>
   );
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 // eslint-disable-next-line
-export default ({name, year}) => (
+export default ({name, year, children}) => (
   <div>
     <h3>Car name: {name}</h3>
     <p>Year: <strong>{year}</strong></p>
+    { children }
   </div>
 )
