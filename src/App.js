3b0a6b7a3f3e736124df928b7f92065829729093
Andrew Sova <asovacloud@gmail.com>
2020-11-18 09:45:51 +0200

    Refuctoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from  'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Yo, buddy...</h1>
+    <div>
+      <div className="App">
+        <h1>Yo, buddy...</h1>
+      </div>
+      <p>Hello</p>
     </div>
   );
 
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App'
-  //   },
-  //   React.createElement(
-  //     'h1',
-  //     null,
-  //     'Yo, buddy!!!!'
-  //   )
-  // );
 }
 
 export default App;
