c7b0b6cb077d547787173c7bf1e6f2bbe62a9a8f
AndrewSova <asovacloud@gmail.com>
2020-11-23 07:21:58 +0200

    Create CSS module

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
+import './Car.css';
 // eslint-disable-next-line
 export default ({name, year, onChangeName, onDelete}) => (
-    <div style={{
-        border: '1px solid #ccc',
-        margin: '0.8rem 0',
-        padding: '0.8rem',
-        boxShadow: '0 4px 5px rgba(0,0,0,0.14)',
-        borderRadius: '5px'
-    }}>
+    <div className="Car">
         <h3>Car name: {name}</h3>
         <p>Year: <strong>{year}</strong></p>
         <input type="text" onChange={onChangeName} value={name} />
