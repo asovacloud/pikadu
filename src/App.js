5b471bdbb48ecf3de33eaabd24ca87556ff754a7
AndrewSova <asovacloud@gmail.com>
2020-11-28 16:39:36 +0200

    Start redux

--- src/App.js
+++ src/App.js
@@ -1,93 +1,29 @@
 import React, { Component } from 'react';
-import Car from './Car/Car';
-
 import './App.scss';
 
 class App extends Component {
 
-  constructor(props) {
-    console.log('App constructor');
-    super(props);
-
-    this.state = {
-      cars: [
-        { name: 'BMW', year: 2021 },
-        { name: 'Audi A8', year: '2019' },
-        { name: 'Mazda', year: 2010 },
-        { name: 'Suzuki', year: 2021 }
-      ],
-      pageTitle: 'Yo, buddy.',
-      showCars: false
-    }
+  state = {
+    counter: 0
   }
 
-  changeNameHandler = (name, idx) => {
-    this.setState(state => {
-      const cars = state.cars.concat();
-      cars[idx].name = name;
-      return {
-        cars
-      }
-    });
-  };
-
-  toggleCarsHandler = () => this.setState(state => {
-    return {
-      showCars: !state.showCars
-    }
-  });
-
-  deleteHandler = (index) => {
-    this.setState(state => {
-      const cars = state.cars.concat();
-      cars.splice(index, 1);
-
-      return { cars }
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
     })
-  };
-
-  componentWillMount() {
-    console.log('App componentWillMount');
-  }
-
-  componentDidMount() {
-    console.log('App componentDidMount')
   }
 
   render() {
-    console.log('App render');
-    const divStyle = {
-      textAlign: 'center'
-    }
-
-    const cars = this.state.cars.map((car, idx) => {
-      return (
-        <Car
-          key={idx}
-          name={car.name}
-          year={car.year}
-          onDelete={this.deleteHandler.bind(null, idx)}
-          onChangeName={event => {
-            return this.changeNameHandler(event.target.value, idx)
-          }} />
-      );
-    });
-  
     return (
-      <div style={divStyle}>
-        {/*<h1>{this.state.pageTitle}</h1>*/}
-        <h1>{ this.props.title }</h1>
+      <div className={'App'}>
+        <h1>Counter <strong>{this.state.counter}</strong></h1>
 
-        <button onClick={this.toggleCarsHandler}>Toggle Cars</button>
+        <hr/>
   
-        { this.state.showCars && (
-            <div style={{
-              maxWidth: '19rem',
-              margin: '0 auto'
-            }}>
-              {cars}
-            </div>
-        ) }
+        <div className="Actions">
+          <button onClick={() => this.updateCounter(1)}>Increment 1</button>
+          <button onClick={() => this.updateCounter(-1)}>Decrement 1</button>
+        </div>
       </div>
     );
   }
