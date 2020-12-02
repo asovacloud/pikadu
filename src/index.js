b92889ad00220a983c7760cab3a0773fad3fabda
AndrewSova <asovacloud@gmail.com>
2020-12-02 20:09:18 +0200

    Create Middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import rootReducer from './redux/rootReducer';
 import './index.scss';
 
-const store = createStore(rootReducer);
+/*function loggerMiddleware(store) {
+  return function (next) {
+    return function (action) {
+      const result = next(action);
+
+      console.log("Middleware", store.getState());
+      return result;
+    }
+  }
+}*/
+
+const loggerMiddleware = store => next => action => {
+  const result = next(action);
+
+  console.log("Middleware: ", store.getState());
+  return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
     <Provider store={store}>
