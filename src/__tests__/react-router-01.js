a2bdb0ab66c7daa6978e1b0115141a934af4f353
AndrewSova <asovacloud@gmail.com>
2021-01-09 12:39:43 +0200

    Add React Router

--- src/__tests__/react-router-01.js
+++ src/__tests__/react-router-01.js
@@ -6,12 +6,14 @@ import {Main} from '../main'
 
 test('main renders about and home and I can navigate to those pages', () => {
   const history = createMemoryHistory({initialEntries: ['/']})
-  const {getByRole, getByText} = render(
+  const {getByRole, getByText, debug} = render(
     <Router history={history}>
       <Main />
     </Router>,
   )
   expect(getByRole('heading')).toHaveTextContent(/home/i)
+  debug()
   fireEvent.click(getByText(/about/i))
   expect(getByRole('heading')).toHaveTextContent(/about/i)
+  debug()
 })
