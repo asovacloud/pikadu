d75c872c0eb15d7cd90f862b5d845c9f5ea98c30
AndrewSova <asovacloud@gmail.com>
2021-01-14 22:52:26 +0200

    Change message

--- src/__tests__/custom-hook-04.js
+++ src/__tests__/custom-hook-04.js
@@ -24,7 +24,7 @@ test('allows customization of the step', () => {
   expect(result.current.count).toBe(0)
 })
 
-test('the step can be changed and updated', () => {
+test('the step can be changed and updated and something else', () => {
   const {result, rerender} = renderHook(useCounter, {
     initialProps: {step: 3},
   })
