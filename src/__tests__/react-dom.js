49860bda8a275f6f8649ccdc0238de55ad6318c3
AndrewSova <asovacloud@gmail.com>
2021-02-16 09:51:45 +0200

    feat: React dom

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number input with a label "Favorite Number"', () => {
+it('renders a number input with a label FavoriteNumber', () => {
   const div = document.createElement('div')
+
   ReactDOM.render(<FavoriteNumber />, div)
   expect(div.querySelector('input').type).toBe('number')
   expect(div.querySelector('label').textContent).toBe('Favorite Number')
